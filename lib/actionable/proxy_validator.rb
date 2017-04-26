6eacdda0a2c76c0b4a7bfeef2117f5086f6ca33e
Adrian Esteban Madrid <aemadrid@gmail.com>
2017-04-26 13:29:08 -0600

    Adds a (Default) model for proxy validators and makes sure we forward class methods then.

--- lib/actionable/proxy_validator.rb
+++ lib/actionable/proxy_validator.rb
@@ -2,8 +2,36 @@ module Actionable
   class ProxyValidator < SimpleDelegator
     include ActiveModel::Validations
 
-    def self.valid?(instance)
-      new(instance).valid?
+    UniquenessValidator = ActiveRecord::Validations::UniquenessValidator
+
+    class << self
+      def default_model_name
+        name.gsub(/Validator$/, '')
+      end
+
+      def model
+        @model || default_model_name.constantize
+      end
+
+      def set_model(klass)
+        @model = klass
+      end
+
+      def valid?(instance)
+        new(instance).valid?
+      end
+
+      def method_missing(m, *args, &block)
+        if model.respond_to?(m)
+          model.__send__(m, *args, &block)
+        else
+          super(m, *args, &block)
+        end
+      end
+
+      def respond_to_missing?(m, include_private)
+        model.respond_to?(m, include_private) || super
+      end
     end
 
     private
