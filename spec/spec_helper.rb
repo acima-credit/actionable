51636eb906fb74d01ed7b9cc9b58557356797956
Adrian Esteban Madrid <aemadrid@gmail.com>
2017-09-07 11:49:37 -0600

    Adds formatters for results.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,13 @@ require 'support/actionable'
 ENV['ACTIONABLE_BACKTRACE_QTY']   ||= '3'
 ENV['ACTIONABLE_SHORT_BACKTRACE'] ||= 'true'
 
+require 'rspec/core/formatters/base_text_formatter'
+
 RSpec.configure do |config|
   config.example_status_persistence_file_path = '.rspec_status'
+  config.add_formatter :json, 'spec/results/examples.json'
+  config.add_formatter :documentation, 'spec/results/examples.txt'
+  config.add_formatter :html, 'spec/results/examples.html'
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
