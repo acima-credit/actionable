97864c4f68f7b3ea73642b3068da698445add25e
Tad Thorley <phaedryx@gmail.com>
2022-09-23 10:02:06 -0600

    fix name

--- lib/actionable/standard_failures.rb
+++ lib/actionable/standard_failures.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Actionable
-  module Fail
+  module StandardFailures
     INVALID        = :invalid
     NOT_FOUND      = :not_found
     UNAUTHORIZED   = :unauthorized
